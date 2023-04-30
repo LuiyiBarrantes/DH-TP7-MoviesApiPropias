d0b2e7f7ba5872065d2df8f099cecd2a5af23e2f
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-04-30 19:16:25 -0300

    arreglo ruta cambio el orden

--- src/routes/api/actorsRoutes.js
+++ src/routes/api/actorsRoutes.js
@@ -4,7 +4,9 @@ const actorsController = require('../../controllers/api/actorsController');
 
 /* /api */
 router.get('/actors', actorsController.list);
+router.get('/actors/recommended', actorsController.recomended);
 router.get('/actors/:id', actorsController.detail);
+
 router.post('/actors', actorsController.store);
 router.post('/actors/:id', actorsController.update);
 router.delete('/actors/:id', actorsController.destroy);
