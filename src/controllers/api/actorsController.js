a7406e56bee0b93cda3916e897ea489a5c6adb72
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-04-30 19:15:09 -0300

    defino getRecomnded requiero en controller

--- src/controllers/api/actorsController.js
+++ src/controllers/api/actorsController.js
@@ -1,5 +1,5 @@
 //const db = require('../../database/models');
-const { getAllActors, getOneActor } = require('../../services/actorsServices');
+const { getAllActors, getOneActor, getRecomendedActor } = require('../../services/actorsServices');
 //const sequelize = db.sequelize;
 
 
@@ -49,6 +49,29 @@ const actorsController = {
                 msg: error.message
             })
         }     
+    },
+
+    'recomended': async (req, res) => {
+
+        try {
+
+            const {count:total, rows/* :actorsRecommended */} = await getRecomendedActor()
+            return res.status(200).json({
+                ok: true,
+                meta: {
+                    status: 200,
+                    total: total,
+                    url: "/api/actors/recommended"
+                },
+                data: rows/* actorsRecommended */
+            })
+
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                msg: error.message
+            })
+        }
     },
 
     store : async (req, res) => {
--- src/services/actorsServices.js
+++ src/services/actorsServices.js
@@ -67,6 +67,44 @@ module.exports = {
         }
 
     },
+    getRecomendedActor: async (req) => {
+
+        try {
+            const recomendedActor = await db.Actor.findAndCountAll({
+                /**/ include: [
+                    //incluyo la pelicula favorita del actor, solo muestro el titulo
+                    {
+                        association: "favorite_movie",
+                        attributes: ["title"],
+                    },
+                    {
+                        //incluyo las peliculas en las que trabajo, sin los atributos de la tabla pivot
+                        association: "movies",
+                        attributes: ["title"],
+                        through: { attributes: [] }
+                    },
+
+                ],
+                attributes: {
+                    //excluyo los atributos que no necesito mostrar del actor
+                    exclude: ["favorite_movie_id", "created_at", "updated_at"],
+                }, 
+                where: {
+                    rating: { [db.Sequelize.Op.gte]: 8 }
+                },
+                order: [
+                    ['rating', 'DESC']
+                ]
+            })
+            return recomendedActor
+        } catch (error) {
+            throw {
+                status: 500,
+                message: error.message
+            }
+        }
+
+    },
     createActor: async (data) => {
 
         try {
