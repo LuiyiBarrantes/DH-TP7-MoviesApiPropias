dcae741962653b51a3ade5e7824854a4302d92ba
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-04-30 19:12:56 -0300

    arreglo getnewst y defino getrecomended

--- src/services/moviesServices.js
+++ src/services/moviesServices.js
@@ -69,7 +69,7 @@ module.exports = {
     getNewestMovies: async (req) => {
 
         try {
-            const newestMovies = db.Movie.findAndCountAll({
+            const newestMovies = await db.Movie.findAndCountAll({
                 include: [
                     {
                         //inclyo el genero de la pelicula, solo mestro el nombre
@@ -88,12 +88,49 @@ module.exports = {
                 ],
                 attributes: {
                     exclude: ["genre_id", "created_at", "updated_at"],
-                },order: [
+                }, order: [
                     ['release_date', 'DESC']
                 ],
                 limit: 5
             })
             return newestMovies
+        } catch (error) {
+            throw {
+                status: 500,
+                message: error.message
+            }
+        }
+    },
+    getRecomendedMovies: async (req) => {
+        try {
+            const recomendedMovies = await db.Movie.findAndCountAll({
+                include: [
+                    {
+                        //inclyo el genero de la pelicula, solo mestro el nombre
+                        association: "genre",
+                        attributes: ["name"]
+                    },
+                    {
+                        //incluyo los actores de la pelicula, solo muestro nombre y apellido (quise ver si podia concatenar el nombre y apellido pero no supe como), sin los atributos de la tabla pivot
+                        association: "actors",
+                        attributes: ["first_name", "last_name"],
+                        through: { attributes: [] }
+
+                    },
+
+
+                ],
+                attributes: {
+                    exclude: ["genre_id", "created_at", "updated_at"],
+                },
+                where: {
+                    rating: { [db.Sequelize.Op.gte]: 8 }
+                },
+                order: [
+                    ['rating', 'DESC']
+                ]
+            })
+            return recomendedMovies
         } catch (error) {
             throw {
                 status: 500,
