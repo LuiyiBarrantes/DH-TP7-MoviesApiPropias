9bffb152d1b40ecaf81f6ce7b005184f72a45771
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-04-30 02:48:46 -0300

    comento validacion de ranking

--- src/validations/genreValdiator.js
+++ src/validations/genreValdiator.js
@@ -4,9 +4,9 @@ module.exports =
     [check('name')
     .notEmpty()
     .withMessage("El campo 'name' es obligatorio"),
-    check('ranking')
-    .notEmpty()
-    .withMessage("El campo 'ranking' es obligatorio"),
+    //check('ranking')
+    //.notEmpty()
+    //.withMessage("El campo 'ranking' es obligatorio"),
     check('active')
     .notEmpty()
     .withMessage("El campo 'active' es obligatorio"),
