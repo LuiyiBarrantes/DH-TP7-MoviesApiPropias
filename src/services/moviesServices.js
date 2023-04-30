54b6ad350944f6434016478c11f486265d608a52
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-04-30 03:16:58 -0300

    defino GetAllMovies

---
+++ src/services/moviesServices.js
@@ -0,0 +1,69 @@
+const db = require('../database/models');
+
+module.exports = {
+
+    getAllMovies : async (req) => {
+        try {
+            const movies = await db.Movie.findAndCountAll({
+                include: [
+                    {
+                        association: "genre",
+                        attributes:  ["name"] /*
+                        */
+                      },
+                    {
+                      association: "actors",
+                       attributes:  ["first_name","last_name","rating","favorite_movie_id"],
+                      through: { attributes: [] }
+                      /**/
+                    },
+                    
+  
+                  ],
+                attributes: {
+                    exclude: ["genre_id", "created_at", "updated_at","actors.actor_movie"],            
+                  },
+                order: [
+                    ['title','ASC']
+                ]
+            });
+
+            return movies
+        } catch (error) {
+            throw {
+                status : 500,
+                message : error.message
+            }
+        }
+    },
+
+    getOneGenre : async (id) => {
+
+        try {
+            const genre= await db.Genre.findByPk(id);
+            return genre
+        } catch (error) {
+            throw {
+                status : 500,
+                message : error.message
+            }
+        }
+
+    },
+    createGenre : async (data) => {
+
+        try {
+            const newGenre = db.Genre.create({
+                ...data
+            });
+            return newGenre
+        } catch (error) {
+            throw {
+                status : 500,
+                message : error.message
+            }
+        }
+
+    },
+    
+}
